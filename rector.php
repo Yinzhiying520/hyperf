3bac15d5c01d86ae99bd0ead10e679113477e5c7
Ayin <1003103703@qq.com>
2024-08-19 16:46:41 +0800

    test

--- rector.php
+++ rector.php
@@ -9,6 +9,7 @@ declare(strict_types=1);
  * @contact  group@hyperf.io
  * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
  */
+
 use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\Config\RectorConfig;
 use Rector\PHPUnit\Set\PHPUnitSetList;
